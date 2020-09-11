dabfa7f2c7cda721e973778bfc801fa1c3f6f4d9
Hernan Velasquez <=>
2020-09-11 08:20:24 -0500

    Commit inicial, curso de Lenguaje C

---
+++ cmake-build-debug/CMakeFiles/CursoLenguajeC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CursoLenguajeC.dir/main.c.obj"
+  "CursoLenguajeC.exe"
+  "CursoLenguajeC.exe.manifest"
+  "CursoLenguajeC.pdb"
+  "libCursoLenguajeC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CursoLenguajeC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
